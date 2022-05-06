ccd156e980286c5ab289fe1a1e04067fd61ad5bc
sir-sa <mngumi44@gmail.com>
2022-05-06 15:29:46 +0300

    updated

--- sketch.js
+++ sketch.js
@@ -1,12 +1,28 @@
+// function setup() {
+//   createCanvas(400, 400);
+// }
+
+// function draw() {
+//   background(220, 0, 200);
+  
+//   rectMode(CENTER);
+//   fill(0, 255, 2);
+//   stroke(0,100,100);
+//   rect(200, 150, 150, 150, 20);
+// }
+
+let circleX =100;
 function setup() {
   createCanvas(400, 400);
+   
 }
 
 function draw() {
-  background(220, 0, 200);
-  
-  rectMode(CENTER);
+  background(0);
+
   fill(0, 255, 2);
   stroke(0,100,100);
-  rect(200, 150, 150, 150, 20);
+  circle(circleX, 150, 75, 75);
+  
+  circleX ++;
 }
